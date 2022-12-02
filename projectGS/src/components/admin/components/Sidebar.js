64b4c79b8b1ea5342cb8fbbb22667796c4f1ae41
ksw3108 <ksw3108@naver.com>
2022-12-02 12:12:47 +0900

    221202 선우 업데이트
    관리자페이지 사이드의 메뉴 추가(상품권 관리)
    포인트 이력의 컴포넌트 분리
    사용자 페이지 공지사항 추가
    관리자페이지 - 사용자 관리 상세보기 페이지에 포인트 내역 추가

--- projectGS/src/components/admin/components/Sidebar.js
+++ projectGS/src/components/admin/components/Sidebar.js
@@ -5,6 +5,7 @@ import LaunchIcon from '@mui/icons-material/Launch';
 import InventoryIcon from '@mui/icons-material/Inventory';
 import ContactPageIcon from '@mui/icons-material/ContactPage';
 import ContentPasteIcon from '@mui/icons-material/ContentPaste';
+import MoneyIcon from '@mui/icons-material/Money';
 // import SettingsIcon from "@mui/icons-material/Settings";
 import CampaignIcon from '@mui/icons-material/Campaign';
 import HouseIcon from '@mui/icons-material/House';
@@ -14,7 +15,7 @@ import TopBar from '../not_using/TopBar';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useRef } from 'react';
 
-import "../../../css/admin/common.scss";
+import '../../../css/admin/common.scss';
 
 const Sidebar = () => {
   const navigate = useNavigate();
@@ -25,30 +26,80 @@ const Sidebar = () => {
         <div className="sidebarMenu">
           <ul className="sidebarList">
             <li className="sidebarListItem">
-              <button onClick={() => {window.open('/');}}><LaunchIcon />사이트 바로가기</button>
+              <button
+                onClick={() => {
+                  window.open('/');
+                }}
+              >
+                <LaunchIcon />
+                사이트 바로가기
+              </button>
             </li>
           </ul>
 
           <ul className="sidebarList">
             <li className="sidebarListItem">
-              <button onClick={() => {navigate('/admin');}}><HouseIcon />메인페이지</button>
+              <button
+                onClick={() => {
+                  navigate('/admin');
+                }}
+              >
+                <HouseIcon />
+                메인페이지
+              </button>
             </li>
             <li>
-              <button onClick={() => {navigate('/admin/admininfo');}}><FaceIcon />내 정보 </button>
+              <button
+                onClick={() => {
+                  navigate('/admin/admininfo');
+                }}
+              >
+                <FaceIcon />내 정보{' '}
+              </button>
             </li>
 
             <li className="sidebarListItem">
-              <button onClick={() => {navigate('/admin/boardmanage');}}><CampaignIcon />공지사항</button>
+              <button
+                onClick={() => {
+                  navigate('/admin/boardmanage');
+                }}
+              >
+                <CampaignIcon />
+                공지사항
+              </button>
             </li>
           </ul>
 
-
           <ul className="sidebarList">
             <li className="sidebarListItem">
-              <button onClick={() => {navigate('/admin/disposereport');}}><InventoryIcon />신고 처리</button>
+              <button
+                onClick={() => {
+                  navigate('/admin/disposereport');
+                }}
+              >
+                <InventoryIcon />
+                신고 처리
+              </button>
             </li>
             <li className="sidebarListItem">
-              <button onClick={() => {navigate('/admin/usermanage');}}><ContactPageIcon />회원 관리(미완)</button>
+              <button
+                onClick={() => {
+                  navigate('/admin/usermanage');
+                }}
+              >
+                <ContactPageIcon />
+                회원 관리(미완)
+              </button>
+            </li>
+            <li className="sidebarListItem">
+              <button
+                onClick={() => {
+                  navigate('/admin/goodsmanage');
+                }}
+              >
+                <MoneyIcon />
+                상품권 관리
+              </button>
             </li>
             {/* <li className="sidebarListItem">
               <button
